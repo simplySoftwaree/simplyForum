50bbda50856f59f44f1cdf52bcea3cc3ec3fe60d
simplySoftwaree <82988269+simplySoftwaree@users.noreply.github.com>
2021-04-25 19:26:46 -0400

    Update config.js

--- routes/controller/config.js
+++ routes/controller/config.js
@@ -1,11 +1,11 @@
 module.exports = {
     bannedWords: ["Fuck", "Faggot", "Fag"],
     email:{
-        myEmail: "simplySoftware",
-        host: "simplysoftware.dev",
-        port: 465,
-        secure: true,
-        user: "contact@simplysoftware.dev",
-        pass: "_!MirRbOWKE;"
+        myEmail: "",
+        host: "",
+        port: ,
+        secure: ,
+        user: "",
+        pass: ";"
     }
 };
